f602bed86f9b98531fe9de982b740ad8b6a272a2
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-04 19:23:31 +0100

    Removing commented our code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,3 @@
-// // import { PrismaClient } from "@prisma/client";
-// import { PrismaClient } from "@/generated/prisma";
-// import { withAccelerate } from "@prisma/extension-accelerate";
-
-// const globalForPrisma = global as unknown as { prisma: typeof prisma };
-
-// const prisma =
-// 	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// export default prisma;
-
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
