34c444ecbe11c2f87f24735c3bcd021b479c4b51
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-04 19:26:13 +0100

    Importing invoice count from getInvoices function and so greying out Next button when there are no more invoices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,69 +18,76 @@ export default async function Home({
 
 	const skip = (page - 1) * limit;
 
-	const invoices = await getInvoices(skip, limit);
+	const { invoices, numberOfInvoices } = await getInvoices(skip, limit);
 
 	return (
-		<div className="w-4/5 min-h-2/3 mx-auto mt-20">
-			<table className="min-w-full text-left text-sm font-light">
-				<thead className="border-2 border-neutral-200 bg-white font-medium">
-					<tr className="bg-blue-200">
-						<th className="px-6 py-4">Date</th>
-						<th className="px-6 py-4">Customer</th>
-						<th className="px-6 py-4">Email</th>
-						<th className="px-6 py-4">Status</th>
-						<th className="px-6 py-4">Amount</th>
-					</tr>
-				</thead>
-				<tbody>
-					{invoices.map((invoice) => (
-						<tr
-							key={invoice.id}
-							className="border-2 border-neutral-200 bg-whote"
-						>
-							<td className="whitespace-nowrap px-6 py-4">
-								{new Date(invoice.date).toLocaleDateString()}
-							</td>
-							<td className="whitespace-nowrap px-6 py-4">
-								{invoice.fullName}
-							</td>
-							<td className="whitespace-nowrap px-6 py-4">{invoice.email}</td>
-							<td className="whitespace-nowrap px-6 py-4">
-								<Status
-									className={cn(
-										"capitalize",
-
-										invoice.status === "OPEN" && "bg-blue-400",
-										invoice.status === "PAID" && "bg-green-500",
-										invoice.status === "CANCELLED" && "bg-zinc-500",
-										invoice.status === "UNCOLLECTABLE" && "bg-red-500"
-									)}
-								>
-									{invoice.status}
-								</Status>
-							</td>
-							<td className="whitespace-nowrap px-6 py-4">$5000</td>
+		<main className="">
+			<h1 className="text-4xl font-bold ml-5 mt-7">Invoice Reporting</h1>
+			<div className="w-4/5 min-h-2/3 mx-auto mt-20">
+				<table className="min-w-full text-left text-sm font-light">
+					<thead className="border-2 border-neutral-200 bg-white font-medium">
+						<tr className="bg-blue-200">
+							<th className="px-6 py-4">Date</th>
+							<th className="px-6 py-4">Customer</th>
+							<th className="px-6 py-4">Email</th>
+							<th className="px-6 py-4">Status</th>
+							<th className="px-6 py-4">Amount</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-			<div className="flex gap-2 justify-end mt-3 text-md">
-				<Link
-					href={`/?page=${page > 1 ? page - 1 : 1}&limit=${limit}`}
-					className={clsx(
-						"mr-4 rounded-md bg-slate-200 px-2",
-						page <= 1 && "cursor-not-allowed font-light text-gray-400"
-					)}
-				>
-					Previous{" "}
-				</Link>
-				<Link
-					href={`/?page=${page + 1}&limit=${limit}`}
-					className={clsx("mr-4 rounded-md bg-slate-200 px-2")}
-				>
-					Next{" "}
-				</Link>
+					</thead>
+					<tbody>
+						{invoices.map((invoice) => (
+							<tr
+								key={invoice.id}
+								className="border-2 border-neutral-200 bg-whote"
+							>
+								<td className="whitespace-nowrap px-6 py-4">
+									{new Date(invoice.date).toLocaleDateString()}
+								</td>
+								<td className="whitespace-nowrap px-6 py-4">
+									{invoice.fullName}
+								</td>
+								<td className="whitespace-nowrap px-6 py-4">{invoice.email}</td>
+								<td className="whitespace-nowrap px-6 py-4">
+									<Status
+										className={cn(
+											"capitalize",
+
+											invoice.status === "OPEN" && "bg-blue-400",
+											invoice.status === "PAID" && "bg-green-500",
+											invoice.status === "CANCELLED" && "bg-zinc-400",
+											invoice.status === "UNCOLLECTABLE" && "bg-red-500"
+										)}
+									>
+										{invoice.status}
+									</Status>
+								</td>
+								<td className="whitespace-nowrap px-6 py-4">$5000</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+				<div className="flex gap-2 justify-end mt-3 text-md">
+					<Link
+						href={`/?page=${page > 1 ? page - 1 : 1}&limit=${limit}`}
+						className={clsx(
+							"mr-4 rounded-md bg-slate-200 px-2",
+							page <= 1 && "cursor-not-allowed font-light text-gray-400"
+						)}
+					>
+						Previous{" "}
+					</Link>
+					<Link
+						href={`/?page=${page + 1}&limit=${limit}`}
+						className={clsx(
+							"mr-4 rounded-md bg-slate-200 px-2",
+							page >= numberOfInvoices / limit &&
+								"cursor-not-allowed font-light text-gray-400"
+						)}
+					>
+						Next{" "}
+					</Link>
+				</div>
 			</div>
-		</div>
+		</main>
 	);
 }
