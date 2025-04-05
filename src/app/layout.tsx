3a14c47547a847b3ca8ba637ded2fa6914df04b4
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-05 18:18:37 +0100

    Adding Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +15,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`antialiased bg-slate-300`}>{children}</body>
+			<body className={`antialiased bg-slate-300`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
