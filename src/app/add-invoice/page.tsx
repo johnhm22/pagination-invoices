569ee2e544507403d84c0958cb5915d5f4df0821
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-07 16:29:16 +0100

    Adding cancel button to add-invoice page and updating tailwindcss

--- src/app/add-invoice/page.tsx
+++ src/app/add-invoice/page.tsx
@@ -1,6 +1,7 @@
 import Form from "next/form";
 
 import { addInvoice } from "@/src/data-access/invoices";
+import Link from "next/link";
 
 const page = () => {
 	return (
@@ -8,7 +9,7 @@ const page = () => {
 			<h1 className="text-4xl font-bold ml-5 mt-7">Add Invoice</h1>
 			<Form
 				action={addInvoice}
-				className="relative p-5 pt-7 bg-slate-100 rounded-lg w-1/2 min-h-2/3 mx-auto mt-20 flex flex-col gap-11 border border-slate-200"
+				className="p-5 pt-7 bg-slate-100 rounded-lg w-1/2 min-h-2/3 mx-auto mt-20 flex flex-col gap-11 border border-slate-200"
 			>
 				<div className="">
 					<label htmlFor="name">
@@ -39,9 +40,14 @@ const page = () => {
 						className="hover:shadow-lg mb-10 border border-slate-300 rounded-md ml-7 pl-2 py-1 w-1/2 focus:bg-slate-100"
 					/>
 				</label>
-				<button className="absolute bottom-3 right-3 w-20 rounded-lg bg-blue-500 text-white">
-					Add
-				</button>
+				<div className="flex flex-row gap-3 justify-end">
+					<button className="w-20 rounded-lg bg-red-500 text-black hover:cursor-pointer hover:shadow-xl">
+						<Link href="/">Cancel</Link>
+					</button>
+					<button className="w-20 rounded-lg bg-blue-500 text-black hover:cursor-pointer hover:shadow-xl">
+						Add
+					</button>
+				</div>
 			</Form>
 		</main>
 	);
