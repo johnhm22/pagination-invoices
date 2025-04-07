884f7e8cbf75aac796a0b5edd416d26b21c14484
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-07 15:59:56 +0100

    Adding add and delete invoice functions using Prisma to interface with the db

--- src/data-access/invoices.ts
+++ src/data-access/invoices.ts
@@ -1,4 +1,9 @@
+"use server";
+
+import { redirect } from "next/navigation";
 import { prisma } from "../lib/prisma";
+
+const limit = 5;
 
 export const getInvoices = async (skip: number, limit: number) => {
 	// add authentication here
@@ -10,4 +15,32 @@ export const getInvoices = async (skip: number, limit: number) => {
 	const numberOfInvoices = await prisma.invoice.count();
 
 	return { invoices, numberOfInvoices };
+};
+
+export const addInvoice = async (formData: FormData) => {
+	const name = formData.get("name") as string;
+	const email = formData.get("email") as string;
+	const amount = formData.get("amount") as string;
+
+	await prisma.invoice.create({
+		data: {
+			fullName: name,
+			email,
+			amount,
+			status: "OPEN",
+		},
+	});
+
+	const numberOfInvoices = await prisma.invoice.count();
+
+	redirect(`/?page=${Math.ceil(numberOfInvoices / limit)}&limit=${limit}`);
+};
+
+export const deleteInvoice = async (id: number) => {
+	await prisma.invoice.delete({
+		where: {
+			id,
+		},
+	});
+	redirect(`/`);
 };
