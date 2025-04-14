35356246ee0437e02c2ddd2779622e36b59adaa6
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-14 09:35:02 +0100

    Deleting commented out code and unnecessary className

--- src/components/InvoiceDetails.tsx
+++ src/components/InvoiceDetails.tsx
@@ -6,8 +6,6 @@ import Link from "next/link";
 const InvoiceDetails = ({ id }: { id: number }) => {
 	return (
 		<div>
-			{/* <div className="hover:cursor-pointer"> */}
-			{/* <div className="inline-block relative object-center h-full w-full object-contain p-1"> */}
 			<div className="h-9 w-12 p-1">
 				<Link href={`/invoices/${id}`}>
 					<Image
@@ -16,7 +14,6 @@ const InvoiceDetails = ({ id }: { id: number }) => {
 						height={20}
 						object-fit="none"
 						alt="magnifying-glass"
-						className="inline-block relative object-center h-full w-full object-contain p-1"
 					/>
 				</Link>
 			</div>
