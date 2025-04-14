4b90a699edafa4600d95884bf915e45fc0fb6975
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-14 11:39:44 +0100

    Changing Status component to simple button Updating tailwindcss

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,9 +1,8 @@
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
-import { Status } from "@/src/components/Status";
 import { getSingleInvoice } from "@/src/data-access/invoices";
 import { cn } from "@/src/lib/utils";
-import Link from "next/link";
 
 export default async function Page({
 	params,
@@ -22,20 +21,23 @@ export default async function Page({
 		notFound();
 	}
 
-	console.log("invoiceData: ", invoice);
-
-	// console.log("invoice id: ", invoiceId);
 	return (
-		<main className="min-h-screen">
-			<div className="min-h-2/3 ml-10 mt-20 w-1/3 border border-red-500">
-				{/* <h1 className="text-4xl font-bold mt-7">Invoice Details</h1> */}
-				<div className="flex flex-row gap-11 py-5 mb-5">
-					<p className="text-3xl font-semibold">
+		<main className="flex justify-center items-center min-h-screen">
+			<div className="bg-slate-200 rounded-tr-lg rounded-bl-lg flex flex-col w-4/5 md:w-2/3 lg:w-2/5 px-2">
+				<div className="flex flex-row justify-around gap-11 py-5 mb-5">
+					<p className="text-lg md:text-xl font-semibold">
 						Invoice Reference: {invoiceId}
 					</p>
-					<Status
+					<button
+						type="button"
 						className={cn(
 							"capitalize",
+							"rounded-lg",
+							"px-3",
+							"py-2",
+							"text-center",
+							"text-sm",
+							"size-max",
 
 							invoice.status === "OPEN" && "bg-blue-400",
 							invoice.status === "PAID" && "bg-green-500",
@@ -44,18 +46,21 @@ export default async function Page({
 						)}
 					>
 						{invoice.status}{" "}
-					</Status>
+					</button>
 				</div>
 				<div className="flex flex-col gap-7">
 					<p className="text-xl font-semibold">{invoice.fullName}</p>
 					<p className="text-xl font-semibold">{invoice.email}</p>
+					<p className="text-xl font-semibold">£{invoice.amount}</p>
 					<p className="text-xl font-semibold">
 						{new Date(invoice.date).toLocaleDateString()}
 					</p>
 				</div>
-				<button className=" border border-red-500 w-20 rounded-lg bg-blue-500 text-black hover:cursor-pointer hover:shadow-xl">
-					<Link href="/">Back</Link>
-				</button>
+				<div className="flex justify-end">
+					<button className="bg-blue-400 py-1 my-3 mr-2 w-20 rounded-lg text-black hover:cursor-pointer hover:shadow-xl">
+						<Link href="/">Back</Link>
+					</button>
+				</div>
 			</div>
 		</main>
 	);
