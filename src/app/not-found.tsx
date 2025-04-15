2ff449c8a85846fac094f98a1bfcf89e88c2d020
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-15 15:58:05 +0100

    Adding 404 pages for root and [invoiceId]

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NotFound() {
+	return (
+		<div className="rounded-lg p-5 mx-auto mt-16 min-h-fit w-1/2 bg-slate-100 items-center text-center">
+			<h1 className="text-6xl md:text-9xl font-semibold">404</h1>
+			<h2 className="text-xl md:text-4xl mt-9 mb-9">
+				Apologies, that url link is unknown to us.
+			</h2>
+
+			<Link className="bg-blue-500 rounded-lg px-3 py-2 mb-7" href="/">
+				Return Home
+			</Link>
+		</div>
+	);
+}
